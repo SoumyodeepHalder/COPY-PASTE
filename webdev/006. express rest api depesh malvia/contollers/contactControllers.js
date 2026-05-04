37e27156692307eca62c40f0a9982ff36dcc9358
SoumyodeepHalder <92792771+SoumyodeepHalder@users.noreply.github.com>
2026-05-04 21:32:04 +0530

    Add files via upload

---
+++ webdev/006. express rest api depesh malvia/contollers/contactControllers.js
@@ -0,0 +1,22 @@
+const getContacts = (req, res) => {
+    res.status(200).json({ message: 'get all contacts' });
+};
+const createContact = (req, res) => {
+    res.status(201).json({ message: 'create contact' });
+};
+const getContact = (req, res) => {
+    res.status(200).json({ message: `get contact for ${req.params.id}` });
+};
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `update contact for ${req.params.id}` });
+};
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `delete contact for ${req.params.id}` });
+};
+module.exports = { 
+    getContacts, 
+    createContact, 
+    getContact, 
+    updateContact, 
+    deleteContact 
+};
