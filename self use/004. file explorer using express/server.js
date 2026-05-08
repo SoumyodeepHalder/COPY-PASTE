9d0d2d9f16f2cb2c98552d844412e50d25ab3958
SoumyodeepHalder <92792771+SoumyodeepHalder@users.noreply.github.com>
2026-05-08 23:33:50 +0530

    Update server.js

--- self use/004. file explorer using express/server.js
+++ self use/004. file explorer using express/server.js
@@ -1,21 +1,34 @@
-const express=require ('express');
-const app=express ();
+const express = require('express');
+const app = express();
 const fs = require('fs');
-const cors=require ('cors')
+const cors = require('cors')
 
-app.use (express.static ('./public'));
-app.use (express.json ());
+app.use(express.static('./public'));
+app.use(express.json());
 app.use(cors());
 
 app.post('/', (req, res) => {
-    const allContents = fs.readdirSync(req.body.url);
-    res.json(allContents);
-    // console.log(req.body);
-    // res.json ('helo');
-  });
+  let path=req.body.url;
+  if (fs.statSync (path).isDirectory ()){
+    const allContents = fs.readdirSync(path);
+    res.json({
+      "isdir":true,
+      "content":allContents
+    });
+  }
+  else {
+    // let data=fs.readFileSync (path);
+    res.json ({
+      "isdir":false,
+      "content":path
+    });
+  }
+  // console.log(req.body);
+  // res.json ('helo');
+});
 
-app.listen (3000, ()=> {
-    console.log('server started');
+app.listen(3000, () => {
+  console.log('server started');
 });
 
 
