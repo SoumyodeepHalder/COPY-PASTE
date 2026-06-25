053b80563abb9df0ea11055449de2bab7791bde7
SoumyodeepHalder <92792771+SoumyodeepHalder@users.noreply.github.com>
2026-06-25 15:53:47 +0530

    Create errorHandler.js

---
+++ webdev/006. express rest api depesh malvia/middleware/errorHandler.js
@@ -0,0 +1,45 @@
+const {constants}=require ('../constants');
+const errorHandler=(err, req, res, next)=>{
+    const statusCode=res.statusCode ? res.statusCode : 500;
+    switch (statusCode){
+        case constants.VALIDATION_ERROR:
+            res.json ({
+                title: 'validation error', 
+                message: err.message, 
+                stackTrace: err.stack
+            });
+            break;
+        case constants.NOT_FOUND:
+            res.json ({
+                title: 'not found', 
+                message: err.message, 
+                stackTrace: err.stack
+            });
+            break;
+        case constants.UNAUTHARIZED:
+            res.json ({
+                title: 'unauthorized', 
+                message: err.message, 
+                stackTrace: err.stack
+            });
+            break;
+        case constants.FORBIDDEN:
+            res.json ({
+                title: "forbiddenm", 
+                message: err.message, 
+                stackTrace: err.stack
+            });
+        case constants.SERVER_ERROR:
+            res.json ({
+                title: 'server error', 
+                message: err.message, 
+                stackTrace: err.stack
+            });
+            break;
+        default:
+            console.log ('no error all good');
+            break;
+    }
+};
+
+module.exports=errorHandler;
